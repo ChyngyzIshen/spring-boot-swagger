73a4a34e4d9f2f0d04f5b050952c5c6409997526
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2021-11-30 16:58:58 +0600

    config swagger

--- src/main/java/com/peaksoft/springbootswagger/SpringBootSwaggerApplication.java
+++ src/main/java/com/peaksoft/springbootswagger/SpringBootSwaggerApplication.java
@@ -2,7 +2,15 @@ package com.peaksoft.springbootswagger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -10,6 +18,28 @@ public class SpringBootSwaggerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSwaggerApplication.class, args);
+	}
+
+	@Bean
+	public Docket swaggerConfiguration(){
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.ant("/api/*"))
+				.apis(RequestHandlerSelectors.basePackage("com.peaksoft"))
+				.build()
+				.apiInfo(apiDetails());
+	}
+
+	private ApiInfo apiDetails(){
+		return new ApiInfo(
+				"Address book API",
+				"Sample API for Peaksoft students",
+				"1.0",
+				"Free to use",
+				new springfox.documentation.service.Contact("Chyngyz Sharshekeev", "https://peaksoft.school", "sharshekeev.chyngyz@gmail.com"),
+				"API License",
+				"https://peaksoft.school",
+				Collections.emptyList());
 	}
 
 }
--- src/main/java/com/peaksoft/springbootswagger/rest/AddressBookResource.java
+++ src/main/java/com/peaksoft/springbootswagger/rest/AddressBookResource.java
@@ -1,6 +1,8 @@
 package com.peaksoft.springbootswagger.rest;
 
 import com.peaksoft.springbootswagger.model.Contact;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -9,23 +11,27 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 @RestController
-@RequestMapping(path ="/api")
+@RequestMapping(path = "/api")
 public class AddressBookResource {
 
     ConcurrentMap<String, Contact> contacts = new ConcurrentHashMap<>();
 
     @GetMapping("/{id}")
-    public Contact getContact(@PathVariable String id){
+    @ApiOperation(value = "Find contact by id",
+            notes = "Provide an id to look up specific contact from the address book",
+            response = Contact.class)
+    public Contact getContact(@ApiParam(value = "ID value for the contact you need to retrieve", required = true)
+                              @PathVariable String id) {
         return contacts.get(id);
     }
 
     @GetMapping("/")
-    public List<Contact> getAllContacts(){
+    public List<Contact> getAllContacts() {
         return new ArrayList<Contact>(contacts.values());
     }
 
     @PostMapping("/")
-    public Contact addContact(@RequestBody Contact contact){
+    public Contact addContact(@RequestBody Contact contact) {
         contacts.put(contact.getId(), contact);
         return contact;
     }
